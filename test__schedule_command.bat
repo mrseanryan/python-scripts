38d354f6301dc77ef338a4b1cdb7a845644ac939
Sean Ryan <str_ie@yahoo.co.uk>
2011-08-13 11:36:12 +0100

    schedule_comannd.py - fix in line endings of test script

--- test__schedule_command.bat
+++ test__schedule_command.bat
@@ -1,1 +1,4 @@
-python schedule_command.py  at_test_command.bat 1 -yECHO After 1 minute, look in local directory 'temp' to see if the scheduled task worked ...
+python schedule_command.py  at_test_command.bat 1 -y
+
+ECHO After 1 minute, look in local directory 'temp' to see if the scheduled task worked ...
+
