8065ac16720a5d1b1790c6ccbd9ad74922b489fa
Sean Ryan <str_ie@yahoo.co.uk>
2011-10-20 17:22:46 +0100

    diff_by_regex - adding more tests

--- test__diff_by_regex.bat
+++ test__diff_by_regex.bat
@@ -14,3 +14,12 @@ diff_by_regex.py  test_data\diff_by_regex\file_to_compare.txt  test_data\diff_by
 
 echo test diff a file that is same, only shorter: (last line should show as different, because no line ending)
 diff_by_regex.py  test_data\diff_by_regex\file_to_compare_longer.txt  test_data\diff_by_regex\file_to_compare.txt
+
+echo test diff a file that is same, only longer: - with -d
+diff_by_regex.py  test_data\diff_by_regex\file_to_compare.txt  test_data\diff_by_regex\file_to_compare_longer.txt		-d
+
+echo test diff a file that is same, only shorter: (last line should show as different, because no line ending) - with -d
+diff_by_regex.py  test_data\diff_by_regex\file_to_compare_longer.txt  test_data\diff_by_regex\file_to_compare.txt		-d
+
+echo test diff of 2 larger files (same, but second is bigger):
+diff_by_regex.py  test_data\diff_by_regex\IRD_calculation.template.processed.sql.appended		test_data\diff_by_regex\known_good_IRD_calculation.template.processed.sql.appended		-d
