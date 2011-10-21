d1d994db82e5c944629656d01b6243a8a70bef53
Sean Ryan <str_ie@yahoo.co.uk>
2011-10-21 11:51:44 +0100

    process_text_template_from_file.py

--- test__process_text_template_from_file.bat
+++ test__process_text_template_from_file.bat
@@ -1,6 +1,9 @@
 @ECHO OFF
 SETLOCAL
 
+SET DIFF_BY_REGEX=diff_by_regex.py
+REM SET DIFF_BY_REGEX=..\Testing\FileProcessingTests\shared\diff_by_regex.py
+
 SET TEMPLATE_DIR=test_data\templates
 
 SET SOURCE_FILE=%TEMPLATE_DIR%\file_template__source.sql
@@ -12,7 +15,7 @@ process_text_template_from_file.py %SOURCE_FILE%	%TEMPLATE_FILE%	IRD_INSERT_POIN
 IF %ERRORLEVEL% NEQ 0 GOTO SomeErrorOccurred
 
 ECHO  ================= VERIFY OUTPUT ========================
-diff_by_regex.py		test_data\templates\known_good__process_text_template_from_file.output.sql			%OUTPUT_FILE%			-d
+"%DIFF_BY_REGEX%"		test_data\templates\known_good__process_text_template_from_file.output.sql			%OUTPUT_FILE%			-d
 IF %ERRORLEVEL% EQU 0 GOTO TEST_OK
 ECHO TEST_Differences_Found
 REM This is a deliberate error, to trigger error in any calling script.
