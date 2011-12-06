98cd0be9605ec8a0c6ea5f832f5dcfa492cacbc1
str <str@localhost>
2011-12-06 21:30:14 +0000

    adding normalise_matrix.py

--- matrix/normalise_matrix.py
+++ matrix/normalise_matrix.py
@@ -1,6 +1,16 @@
 """
-script to manipulate a matrix
-and normalise it
+script to manipulate a square matrix and normalise it
+
+USAGE:  normalise_matrix <matrix data file> <matrix size> [options]
+
+Dependencies:
+Python 2.6 or higher
+
+The options are:
+[-h help]
+[-t test]
+
+Example: normalise_vector.py out.combined.dat 10
 
 """
 
@@ -9,6 +19,34 @@ and normalise it
 # the determinant of a matrix, is 
 # the product of the elements on the main diagonal top-left to bottom-right
 # minus the product of the elements on the diagonal bottom-left to top-right.
+
+from optparse import OptionParser
+
+import sys
+
+###############################################################
+#usage() - prints out the usage text, from the top of this file :-)
+def usage():
+	print __doc__
+
+###############################################################
+#optparse - parse the args
+parser = OptionParser(usage='%prog <matrix data file> <matrix size> [options]')
+parser.add_option('-t', '--test', dest='testModeOn', action='store_const',
+	 	 	 	const=True, default=False,
+	 	 	 	help='turns on test mode, which does not require a file (default: off)')
+
+matrixFilePath = ''
+matrixSize = 0
+
+(options, args) = parser.parse_args()
+if(not options.testModeOn):
+	if(len(args) != 2):
+		usage()
+		sys.exit(2)
+	else:
+		matrixFilePath = args[0]
+		matrixSize = int(args[1])
 
 # ================ FUNCTIONS ===============================
 
@@ -44,15 +82,24 @@ def getNormalisedMatrix(mat, n):
 			det[i][j] = float(mat[i][j]) / float(divisor)
 	return det
 
-# =========================================================
+def loadMatrix(n, matrixFilePath):
+	mat = None
+	raise('not impl !')
 
-#populate the matrix
-n = 3
-mat = createMatrix(n)
+# =============== MAIN ==========================================
+
+n = matrixSize
+
+mat = None
+if(options.testModeOn):
+	#populate the matrix
+	n = 3
+	mat = createMatrix(n)
+else:
+	mat = loadMatrix(n, matrixFilePath)
 
 print 'matrix: '
 print mat
-
 
 norm = getNormalisedMatrix(mat, n)
 print 'normalised matrix: '
