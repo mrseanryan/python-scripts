ee86e1fb1944e99c05a0ca192683c0826f8c3bdb
str <str_ie@yahoo.co.uk>
2022-05-30 13:07:25 +0200

    Add option to copy only JPEGs

--- copy-selected-images.py
+++ copy-selected-images.py
@@ -27,6 +27,9 @@ parser = OptionParser(usage='%prog <directory> [options]')
 parser.add_option('-d', '--dryrun', dest='is_dry_run', action="store_true",
                   default=False,
                   help='Dry run - no files will be copied')
+parser.add_option('-j', '--jpegonly', dest='is_jpeg_only', action="store_true",
+                  default=False,
+                  help='JPEG only - no RAW files will be copied')
 
 (options, args) = parser.parse_args()
 if(len(args) != 1):
@@ -69,13 +72,16 @@ def copy_files(dir_path):
 
     for filename in filenames:
         copy_file(filename, dir_path)
-        # Sony, RAW, Panasonic
-        raw_file_extensions = ['.ARW', '.RAW', '.RW2']
-        stem = get_stem(filename)
-        for raw_ext in raw_file_extensions:
-            raw_file_path = stem + raw_ext
-            copy_file(raw_file_path, dir_path)
-    print(f"{len(filenames)} file sets found (JPG + raw file(s))")
-
+        if not(options.is_jpeg_only):
+            # Sony, RAW, Panasonic
+            raw_file_extensions = ['.ARW', '.RAW', '.RW2']
+            stem = get_stem(filename)
+            for raw_ext in raw_file_extensions:
+                raw_file_path = stem + raw_ext
+                copy_file(raw_file_path, dir_path)
+    if options.is_jpeg_only:
+        print(f"{len(filenames)} file sets found (JPG file(s) only)")
+    else:
+        print(f"{len(filenames)} file sets found (JPG + raw file(s))")
 
 copy_files(sourceDirPath)
